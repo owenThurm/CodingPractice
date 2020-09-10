d685356540ab910b911f5afe8db5a0c17eb159f9
owenThurm <owen.p.thurm@gmail.com>
2020-09-10 10:23:00 -0400

    problem: determine whether an input string is a repeated substring or not

---
+++ repeatedSubstring.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String sub = "";
+        int subPointer = 0;
+        if(s.length() < 2) return false;
+        while(sub.length() <= s.length()/2) {
+            sub+= Character.toString(s.charAt(subPointer));
+            for(int i=0; i<=s.length()-sub.length(); i+= sub.length()) {
+                if(!s.substring(i, i+sub.length()).equals(sub)) {
+                   break; 
+                } 
+                if(i!=0 && i==s.length()-sub.length()) return true;
+            }
+            subPointer++;
+        }
+        return false;
+    }
+}

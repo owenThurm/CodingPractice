3b7f62e6a045525b70af8802402ad89dcd91673a
owenThurm <owen.p.thurm@gmail.com>
2020-09-29 12:16:29 -0400

    sub-optimal exponential time FIBN solution

---
+++ FIBNExponential.java
@@ -0,0 +1,12 @@
+import java.util.List;
+import java.util.ArrayList;
+
+//shitty exponential time solution
+
+class Solution {
+    public int fib(int n) {
+        if(n == 0) return 0;
+        else if(n == 1) return 1;
+        else return fib(n-1) + fib(n-2);
+    }
+}

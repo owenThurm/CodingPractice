e62867e3a33823b64f45a00aa24ba7d4c213c420
owenThurm <owen.p.thurm@gmail.com>
2020-09-29 10:35:45 -0400

    Calculate the n-th fibonacci number

---
+++ FIBN.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int fib(int n) {
+        if(n == 0) return 0;
+        //n = 3
+        //0 1 1 2
+        List<Integer> fibs = new ArrayList();
+        fibs.add(0);
+        fibs.add(1);
+        for(int i=2; i<=n; i++) fibs.add(fibs.get(i-1) + fibs.get(i-2));
+        return fibs.get(fibs.size()-1);
+    }
+}
